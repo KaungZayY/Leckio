8619f32027718f65c66dffca95ee40edb05b1730
KaungZayY <kaungzayyan19@gmail.com>
2026-04-09 14:57:40 +0700

    tab layout update

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -24,10 +24,10 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="favourites"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: 'My Likes',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="heart.fill" color={color} />,
         }}
       />
     </Tabs>
